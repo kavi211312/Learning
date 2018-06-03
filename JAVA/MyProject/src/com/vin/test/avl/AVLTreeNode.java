4a1601e69d2e348e939305a7cbc46a81b6954db5
vinsyhb <vinay1n1@gmail.com>
2018-06-03 18:58:58 +0530

    AVL Tree insert method

--- JAVA/MyProject/src/com/vin/test/avl/AVLTreeNode.java
+++ JAVA/MyProject/src/com/vin/test/avl/AVLTreeNode.java
@@ -1,15 +1,27 @@
 package com.vin.test.avl;
 
 public class AVLTreeNode {
+	
 	private int data;
 	private AVLTreeNode right;
 	private AVLTreeNode parent;
 	private AVLTreeNode left;
 
+	public AVLTreeNode(int data,AVLTreeNode parent){
+		this.parent = parent;
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+	
 	public int getData() {
 		return data;
 	}
-
+	private void setAsParent(AVLTreeNode child){
+		if(child!=null){
+			child.setParent(this);
+		}
+	}
 	public void setData(int data) {
 		this.data = data;
 	}
@@ -19,7 +31,9 @@ public class AVLTreeNode {
 	}
 
 	public void setRight(AVLTreeNode right) {
+		this.setAsParent(right);
 		this.right = right;
+		
 	}
 
 	public AVLTreeNode getLeft() {
@@ -27,6 +41,7 @@ public class AVLTreeNode {
 	}
 
 	public void setLeft(AVLTreeNode left) {
+		this.setAsParent(left);
 		this.left = left;
 	}
 
@@ -37,5 +52,27 @@ public class AVLTreeNode {
 	public void setParent(AVLTreeNode parent) {
 		this.parent = parent;
 	}
-
+	
+	public int getBalance(){
+		if(this.left != null && this.right != null){
+			return this.right.getHeight()- this.left.getHeight();
+		}else if( this.left != null){
+			return 0- this.left.getHeight();
+		}else if(this.right != null){
+			return this.right.getHeight();
+		}else{
+			return 0;
+		}
+	}
+	public int getHeight(){
+		if( this.left != null && this.right != null){
+			return 1+ Math.max(this.left.getHeight(), this.right.getHeight());
+		}else if(this.left != null){
+			return 1+ this.left.getHeight();
+		}else if(this.right != null){
+			return 1+ this.right.getHeight();
+		}else {
+			return 1;
+		}
+	}
 }
